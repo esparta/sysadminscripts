1a2fdaef0361e84f561f77c3f02b1d00205a1b21
Espartaco Palma <esparta@gmail.com>
2013-01-14 18:20:57 -0600

    Basic MySQL script

---
+++ MySQL/backupmysql.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+## post-clone.sh
+## Developer: Espartaco Palma (esparta@gmail.com)
+## Date: 2013-01-14
+## LastMod: 2013-01-14
+## License: GPL
+## Description: Procedure to backup MySQL servers
+##     Parameters:
+##         database
+##         backupfile
+## Tested on: Ubuntu 11.04 and Ubuntu 12.04
+
+die(){
+  echo >&2 "$@"
+  echo "Usage: backupmysql.sh <Host> <Database> <BackupName>"
+  echo "Sample: backupmysql.sh localhost wordpressblog wpb20130114"
+  exit 1
+}
+
+[ "$#" -eq 3 ] || die "3 arguments required, $# provided"
+mysqldump -u root -h $1 -p $2 | gzip -9 > $3.sql.gz
